74e916cb7c3869fd3ba96871c552c50b021868e7
alexandrutapirdea <tapirdea.alexandru@yahoo.com>
2018-06-04 22:13:12 +0300

    Fixed a bug related to users having different fields

--- src/components/Account.js
+++ src/components/Account.js
@@ -41,15 +41,9 @@ class AccountPage extends Component {
             <AuthUserContext.Consumer>
                 {authUser =>
                     <div>
-                        <h1>Account: {authUser.email}</h1>
-                        {/*<h1>UID: {authUser.uid}</h1>*/}
-                        <h1>Nota IA : {valoriUser.IA}</h1>
-                        <h1>Nota PAO : {valoriUser.PAO}</h1>
-                        <h1>Nota PL : {valoriUser.PL}</h1>
-                        <h1>Nota BD : {valoriUser.BD}</h1>
-                        <h1>Nota MDS : {valoriUser.MDS}</h1>
-                        <h1>Nota Retele : {valoriUser.Retele}</h1>
-
+                        {Object.keys(valoriUser).map(key =>
+                            <h1 key={key}>{key} : {valoriUser[key]}</h1>
+                        )}
                         <PasswordForgetForm/>
                         <PasswordChangeForm/>
 
@@ -58,18 +52,6 @@ class AccountPage extends Component {
             </AuthUserContext.Consumer>
             )}
     }
-//
-// const AccountPage = () =>
-//     <AuthUserContext.Consumer>
-//         {authUser =>
-//             <div>
-//                 <h1>Account: {authUser.email}</h1>
-//                 {console.log(authUser)}
-//                 <PasswordForgetForm />
-//                 <PasswordChangeForm />
-//             </div>
-//         }
-//     </AuthUserContext.Consumer>
 
 const authCondition = (authUser) => !!authUser;
 
